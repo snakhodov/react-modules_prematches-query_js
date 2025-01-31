9b6b9cf8d117fb396befb80e8cca4960d1e9e949
snakhodov <elenraendir1@gmail.com>
2025-01-31 10:53:57 +0200

    fix

--- src/store-provider.jsx
+++ src/store-provider.jsx
@@ -4,12 +4,16 @@ import {useParams} from "react-router-dom";
 
 
 export const StoreProviderComponent = inject('prematchesStore')
-(observer(({markUp, prematchesStore: {activeItems}}) => {
+(observer(({markUp, prematchesStore: {activeBranch, activeItems}}) => {
     let {branchId, sportId, categoryId, tournamentId, matchId} = useParams();
 
     useEffect(() => {
         activeItems.setActiveItems({branchId, sportId, categoryId, tournamentId, matchId})
     }, [branchId, sportId, categoryId, tournamentId, matchId]);
+
+    useEffect(() => {
+        activeBranch?.getSports();
+    }, [activeBranch]);
 
     return markUp;
 }));
